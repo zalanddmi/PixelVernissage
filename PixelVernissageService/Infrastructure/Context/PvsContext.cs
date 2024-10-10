a05181990f2307ba9d6130b9f11f388a68a42b34
zalanddmi <zalanddmi@gmail.com>
2024-10-10 12:30:14 +0500

    feat: добавлена реализация репозиториев

---
+++ PixelVernissageService/Infrastructure/Context/PvsContext.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Interfaces.Entities;
+using Domain.Interfaces.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class PvsContext(DbContextOptions options, ICurrentUserService currentUserService) : DbContext(options)
+    {
+        private readonly ICurrentUserService _currentUserService = currentUserService;
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is AuditableEntity && (
+                        e.State == EntityState.Added
+                        || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((AuditableEntity)entityEntry.Entity).CreatedAt = DateTime.UtcNow;
+                    ((AuditableEntity)entityEntry.Entity).CreatedBy = _currentUserService.CurrentUserId;
+                }
+                else
+                {
+                    Entry((AuditableEntity)entityEntry.Entity).Property(p => p.CreatedAt).IsModified = false;
+                    Entry((AuditableEntity)entityEntry.Entity).Property(p => p.CreatedBy).IsModified = false;
+                }
+                ((AuditableEntity)entityEntry.Entity).ModifiedAt = DateTime.UtcNow;
+                ((AuditableEntity)entityEntry.Entity).ModifiedBy = _currentUserService.CurrentUserId;
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ PixelVernissageService/PixelVernissage.Server/Services/CurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Interfaces.Services;
+using System.Security.Claims;
+
+namespace Server.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public long? CurrentUserId { get; }
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            bool f = long.TryParse(httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier), out long result);
+            CurrentUserId = f ? result : null;
+        }
+    }
+}

b839b0903d6eb9c77fce0ab6771d75b457ece159
Kot4pa <beudydg835gd@gmail.com>
2024-10-15 15:17:42 +0500

    front

--- pixel-vernissage-client/src/App.vue
+++ pixel-vernissage-client/src/App.vue
@@ -1,47 +1,77 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import postCard from "./components/postCard.vue"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <div class="head">
+      <p>Пиксеный вернитаж</p>
+      <input></input>
+      <button></button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="title">
+      <h2>ГАЛЛЕРЕЯ<br>ДЕБЮТИРУЮЩИХ<br>ХУДОЖНИКОВ</h2>
+      <h3>преврати хобби в работу</h3>
+    </div>
+    <div>
+      <ul>
+        <li>Портрет</li>
+        <li>Пейзаж</li>
+        <li>Натюрморт</li>
+        <li>Архитектура</li>
+      </ul>
+      <postCard/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  @media (max-width: 480px) {
+    .head{
+      width: 100%;
+      height: 50px;
+      color: #6F484D;
+      display: flex;
+      align-items: center;
+    }
+    p{
+      font-size: 20px;
+      font-family: Merriweather;
+      margin-left: 5px;
+    }
+    input{
+      width: 5px;
+      margin-left: 160px;
+    }
+    button{
+      margin-right: 13px;
+      margin-left: auto;
+    }
+    .title{
+      background-color: #ede4dd;
+      width: 100%;
+      height: 160px;
+      text-align: center;
+      padding-top: 1px;
+      font-family: Merriweather;
+      color: #6B625D;
+    }
+    h3{
+      font-family: Merriweather;
+    }
+    ul{
+      list-style-type: none;
+    }
+    li{
+      display: inline;
+      margin-right: 10px;
+      font-family: Merriweather;
+      font-size: 16px;
+      color: #6B625D;
+    }
+    li:hover{
+      font-weight: bold;
+      text-decoration: underline;
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ pixel-vernissage-client/src/components/postCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <img @click="" src="../testphoto/ee2a265d6653bc8746b93dacad536bcb.png">
+        <p>Природа акрил Арт вечер ...</p>
+        <div>
+            <p>Евгения</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    div{
+        margin: 20px auto;
+        display: flex;
+        justify-content: center;
+        width: 300px;
+        background-color: white;
+        flex-direction: column;
+    }
+    img{
+        width: 100%;
+    }
+    p{
+        margin: 3px auto;
+        font-family: Merriweather;
+        font-size: 18px;
+        color: #6B625D;
+    }
+</style>
